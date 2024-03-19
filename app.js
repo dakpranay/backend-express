cd285ab2f5154714501f99c2ef54693f7358d464
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-03-19 18:13:20 +0530

    module 6 video 6 build simple api to get data from the predefined folder

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express')
+const fs=require('fs')
 const port = 8080
 
 
 const app = express()
 
+const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+app.get('/api/v1/tours',(req,res)=>{
+    res.status(200).json({
+        status:"success",
+        results:tours.length,
+        data:{
+            tours
+        }
+    })
 
-app.get('/', (req, res) => {
-    res
-        .status(200)
-        .send('Nice to have you')
 })
-
 
 
 app.listen(port, () => {
