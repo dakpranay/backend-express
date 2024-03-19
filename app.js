0f5aeea59ac6b3e1285e897d50a68aabff5d5ecc
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-03-19 19:01:00 +0530

    module 6 video 8 how to read params from the url

--- app.js
+++ app.js
@@ -17,6 +17,26 @@ app.get('/api/v1/tours',(req,res)=>{
     })
 })
 
+app.get('/api/v1/tours/:id',(req,res)=>{
+    const id=req.params.id*1;
+    const tour=tours.find(el=> el.id===id)
+
+    // if(id>tours.length)
+    if(!tour){
+        return res.status(400).json({
+            status:"fail",
+            message:"invalid id"
+        })
+    }
+
+    res.status(200).json({
+        status:"success",
+        data:{
+            tour
+        }
+    })
+})
+
 app.post('/api/v1/tours',(req,res)=>{
     // console.log(req.body)
     const newId=tours[tours.length-1].id+1
