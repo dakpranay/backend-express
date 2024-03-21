1d384ef4aa64d8a71e72fd0afca5e4a0b8510e8d
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-03-21 21:58:46 +0530

    module 6 video 9 create a dummy patch api

--- app.js
+++ app.js
@@ -1,56 +1,74 @@
 const express = require('express')
-const fs=require('fs')
+const fs = require('fs')
 const port = 8080
 
 
 const app = express()
 app.use(express.json())
 
-const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-app.get('/api/v1/tours',(req,res)=>{
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
-        status:"success",
-        results:tours.length,
-        data:{
+        status: "success",
+        results: tours.length,
+        data: {
             tours
         }
     })
 })
 
-app.get('/api/v1/tours/:id',(req,res)=>{
-    const id=req.params.id*1;
-    const tour=tours.find(el=> el.id===id)
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id)
 
     // if(id>tours.length)
-    if(!tour){
+    if (!tour) {
         return res.status(400).json({
-            status:"fail",
-            message:"invalid id"
+            status: "fail",
+            message: "invalid id"
         })
     }
 
     res.status(200).json({
-        status:"success",
-        data:{
+        status: "success",
+        data: {
             tour
         }
     })
 })
 
-app.post('/api/v1/tours',(req,res)=>{
+app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body)
-    const newId=tours[tours.length-1].id+1
-    const newTour=Object.assign({id:newId},req.body)
+    const newId = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({ id: newId }, req.body)
 
     tours.push(newTour)
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err =>{
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
-            status:"success",
-            data:{
-                tour:newTour
+            status: "success",
+            data: {
+                tour: newTour
             }
         })
     })
+})
+
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(400).json({
+            status: "fail",
+            message: "invalid id"
+        })
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour: '<updated tour hear>'
+        }
+    })
+
 })
 
 
