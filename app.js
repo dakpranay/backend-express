48c121d15b76ffafc829469e3c68c279198e7d21
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-03-21 22:03:25 +0530

    module 6 video 9 create a dummy delete api

--- app.js
+++ app.js
@@ -71,6 +71,21 @@ app.patch('/api/v1/tours/:id', (req, res) => {
 
 })
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(400).json({
+            status: "fail",
+            message: "invalid id"
+        })
+    }
+
+    res.status(204).json({
+        status: "success",
+        data:null
+    })
+
+})
+
 
 app.listen(port, () => {
     console.log('server running on port : ' + port)
