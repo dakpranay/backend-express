16289fc5eb71e1bafc4a167520df89614b77001f
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-03-23 18:08:39 +0530

    module 6 video 21 what are enviormental variable and how to use them

--- app.js
+++ app.js
@@ -6,17 +6,12 @@ const app = express()
 const tourRouter=require('./routes/tourRoutes')
 const userRouter=require('./routes/userRoutes')
 
-
 //middlewares
-app.use(morgan('dev'))
+if(process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'))
+}
+
 app.use(express.json())
-// app.use(express.static(`${__dirname}/dev-data/templates`)) //to serve static file
-
-
-app.use((req, res, next) => {
-    console.log("hello from middleware")
-    next()
-})
 
 
 app.use('/api/v1/tours', tourRouter)
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+const dotenv=require('dotenv')
+dotenv.config({path:'./config.env'})
+
 const app=require('./app')
-const port = 8080
+//we can set enviornmental variable using terminal by writting 
+//NODE_ENV=development x=20 nodemon server.js
+
+const port = process.env.PORT || 8080
+
 app.listen(port, () => {
     console.log('server running on port : ' + port)
 })
