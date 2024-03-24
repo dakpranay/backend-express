b5d23774c5e5ab890812d7b97d31d49512cc0d7c
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-03-24 16:34:32 +0530

    module 8  video 2 how to connect to the database

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
-const dotenv=require('dotenv')
-dotenv.config({path:'./config.env'})
+const mongoose = require('mongoose')
 
-const app=require('./app')
+const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' })
+
+const app = require('./app')
 //we can set enviornmental variable using terminal by writting 
 //NODE_ENV=development x=20 nodemon server.js
 
 const port = process.env.PORT || 8080
+
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
+
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then((con)=>{
+    console.log('database connected successfully')
+}).catch(err=>{
+    console.log(err)
+})
+
+
 
 app.listen(port, () => {
     console.log('server running on port : ' + port)
