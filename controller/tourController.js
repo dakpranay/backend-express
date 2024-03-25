abaa2e33da8e9bafa014b0c30a53069337b7c2c7
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-03-25 11:21:02 +0530

    module 8  video 13,14 implement the limiting fields data  functionality

--- controller/tourController.js
+++ controller/tourController.js
@@ -20,6 +20,13 @@ exports.getAllTours = async (req, res) => {
             query = query.sort(req.query.sort);
         }
            
+        //fileld limiting
+        if(req.query.fields){
+            const fields=req.query.fields.split(',').join(' ')
+            query=query.select(fields)
+        }else{
+            query=query.select('-__v')
+        }
 
         const tours = await query
         res.status(200).json({
