a5d6fc2fb9394e408fac216f770286243bb94206
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-03-25 13:47:24 +0530

    module 8  video 20 refatoring the code and make it reusable for other projects

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const Tour = require('../models/tourModel')
+const APIFeatures=require('../utils/apiFeatures')
 
 exports.aliasTopTour=(req,res,next)=>{
     req.query.limit='5'
@@ -8,46 +9,53 @@ exports.aliasTopTour=(req,res,next)=>{
     next()
 }
 
+
+
 exports.getAllTours = async (req, res) => {
     try {
         //filtering
-        const queryObj = { ...req.query }
-        const excludedFields = ['page', 'sort', 'limit', 'fields']
-        excludedFields.forEach(el => delete queryObj[el])
+        // const queryObj = { ...req.query }
+        // const excludedFields = ['page', 'sort', 'limit', 'fields']
+        // excludedFields.forEach(el => delete queryObj[el])
 
-        //advance filtering
-        let queryStr = JSON.stringify(queryObj)
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+        // //advance filtering
+        // let queryStr = JSON.stringify(queryObj)
+        // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
 
-        let query = Tour.find(JSON.parse(queryStr));
+        // let query = Tour.find(JSON.parse(queryStr));
 
         // sorting
-        if (req.query.sort) {
-            query = query.sort(req.query.sort);
-        }
+        // if (req.query.sort) {
+        //     const sortBy=req.query.sort.split(',').join(' ')
+        //     query = query.sort(sortBy);
+        // }else{
+        //     query=query.sort('-createdAt')
+        // }
 
         //fileld limiting
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ')
-            query = query.select(fields)
-        } else {
-            query = query.select('-__v')
-        }
+        // if (req.query.fields) {
+        //     const fields = req.query.fields.split(',').join(' ')
+        //     query = query.select(fields)
+        // } else {
+        //     query = query.select('-__v')
+        // }
 
         //pagination
-        const page = req.query.page * 1
-        const limit = req.query.limit * 1 || 10
-        const skip = (page - 1) * limit
+        // const page = req.query.page * 1
+        // const limit = req.query.limit * 1 || 10
+        // const skip = (page - 1) * limit
 
-        query.skip(skip).limit(limit)
+        // query.skip(skip).limit(limit)
 
 
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments()
-            if(skip>=numTours) throw new Error('this page does not exists')
-        }
+        // if (req.query.page) {
+        //     const numTours = await Tour.countDocuments()
+        //     if(skip>=numTours) throw new Error('this page does not exists')
+        // }
 
-        const tours = await query
+
+        const features=new APIFeatures(Tour.find(),req.query).filter().sort().limitFields().paginate()
+        const tours = await features.query
         res.status(200).json({
             status: "success",
             results: tours.length,
