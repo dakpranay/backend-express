30488864c7c6ad9fdc926950289123ad9c15dc78
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-03-26 17:17:55 +0530

    module 8  video 24 what is mongodb middleware and how to use them

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const slugify=require('slugify')
+
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -7,6 +9,7 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true
     },
+    slug:String,
     duration: {
         type: Number,
         required: [true, 'a tour must have a duration']
@@ -66,6 +69,13 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function(){
     return this.duration/7
 })
+
+//run when save and create not when insertMany
+tourSchema.pre('save',function(next){
+    this.slug=slugify(this.name,{lower:true})
+    next()
+})
+
 
 const Tour = mongoose.model('Tour', tourSchema)
 module.exports = Tour
