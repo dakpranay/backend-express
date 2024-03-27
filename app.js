6165d702ae48ccdb6b0f54452f78bde2e7a6c986
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-03-27 23:32:27 +0530

    module 9 video 5 implement a globle error handler

--- app.js
+++ app.js
@@ -18,14 +18,26 @@ app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/user', userRouter)
 
 app.all('*',(req,res,next)=>{
-    res.status(404).json({
-        status:'fail',
-        message:`no route found ${req.originalUrl
-    }`
-    })
-    next()
+    // res.status(404).json({
+    //     status:'fail',
+    //     message:`no route found ${req.originalUrl
+    // }`
+    // })
+    const err=new Error(`no route found ${req.originalUrl}`)
+    err.statusCode=404
+    err.status='fail'
+    next(err)
 })
 
+app.use((err,req,res,next)=>{
+    err.statusCode=err.statusCode || 500
+    err.status=err.status || 'error'
+
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message
+    })
+})
 
 
 
