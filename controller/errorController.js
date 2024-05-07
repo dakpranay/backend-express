2e23d3ce912578cd92719e5384422fabdf51f4b9
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-05-07 22:11:57 +0530

    module 9 video 9 error handling in development and production enviornment

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,10 +1,42 @@
+const sendErrorDev=(err,res)=>{
+    res.status(err.statusCode).json({
+        status:err.status,
+        error:err,
+        message:err.message,
+        stack:err.stack
+    })
+}
+
+const sendErrorProd=(err,res)=>{
+    //operational, trusted error: send message to client
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status:err.status,
+            message:err.message
+        })
+
+        //programming or other unknown error :dont leak error details 
+    }else{
+
+        console.error(err)
+        res.status(500).json({
+            status:'error',
+            message:'Something went wrong'
+        });
+    }
+}
+
+
 module.exports=(err,req,res,next)=>{
     // console.log(err.stack) to see the error stack and where the error is occured
     err.statusCode=err.statusCode || 500
     err.status=err.status || 'error'
 
-    res.status(err.statusCode).json({
-        status:err.status,
-        message:err.message
-    })
+    if(process.env.NODE_ENV==='development'){
+      sendErrorDev(err,res);
+    }else if(process.env.NODE_ENV==='production'){
+        sendErrorProd(err,res)
+    }
+
+    
 }
