d6745a2ebf0e53bf4f05414ba86c649ffd65aed7
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-05-08 23:01:02 +0530

    module 9 video 14 handle uncaught exception

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose')
 
 const dotenv = require('dotenv')
+
+process.on('uncaughtException',err=>{
+    console.log(err.name,err.message)
+    console.log('uncaught Exception shutting down')
+    process.exit(1)
+})
+
 dotenv.config({ path: './config.env' })
 
 const app = require('./app')
