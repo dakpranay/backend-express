278b63fcef101b85615602d55fe8c00cb7b45bbf
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-05-09 23:11:18 +0530

    module 10  video 4 encrypt the password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose =require('mongoose')
 const validator=require('validator')
+const bcrypt=require('bcryptjs')
 
 const userSchema=new mongoose.Schema({
     name:{
@@ -21,9 +22,29 @@ const userSchema=new mongoose.Schema({
     },
     passwordConfirm:{
         type:String,
-        required:[true,'Please confirm your password']
+        required:[true,'Please confirm your password'],
+        validate:{
+            //This only work on create and  save 
+            validator: function(el) {
+                return el===this.password;
+            },
+            message:"Passwords are not the same!"
+        }
     }
 })
+
+userSchema.pre("save",async function(next){
+    //only run this function if password was actually modifeied
+    if(!this.isModified('password')) return next();
+    //hash the password at the code of 12
+    this.password=await bcrypt.hash(this.password,12)
+
+    //delete the password confirm field
+    this.passwordConfirm=undefined;
+    next()
+})
+
+
 
 const User=mongoose.model('User',userSchema)
 module.exports=User
