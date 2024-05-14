725cf0c8e7bb17951b9499e6134cad1af3623513
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-05-14 22:54:08 +0530

    module 10  video 20 implement the rate limiter to limit the number of request per ip address can send

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit=require('express-rate-limit')
 
 const AppError =require('./utils/appError')
 const globleErrorHandler=require('./controller/errorController')
@@ -7,10 +8,17 @@ const tourRouter=require('./routes/tourRoutes')
 const userRouter=require('./routes/userRoutes')
 
 const app = express()
-//middlewares
+// globel middlewares
 if(process.env.NODE_ENV==='development'){
     app.use(morgan('dev'))
 }
+
+const limiter=rateLimit({
+    max:100,
+    windowMs:60*60*1000,
+    message:'Too many request from this ip please try again in an hour!'
+})
+app.use('/api',limiter)
 
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
