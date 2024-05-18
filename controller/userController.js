a01215b33a19c110562b488334f00170041c994e
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-05-18 16:25:30 +0530

    module 11 video 18 add route to fetch the details of the current logged in user

--- controller/userController.js
+++ controller/userController.js
@@ -38,6 +38,7 @@ exports.updateMe=catchAsync(async (req,res,next)=>{
     })
 
 })
+
 exports.deleteMe=catchAsync(async(req,res,next)=>{
     await User.findByIdAndUpdate(req.user.id,{active:false})
     res.status(204).json({
@@ -45,6 +46,11 @@ exports.deleteMe=catchAsync(async(req,res,next)=>{
         data:null
     })
 })
+
+exports.getMe=(req,res,next)=>{
+    req.params.id=req.user.id;
+    next()
+}
 
 exports.createUser = (req, res) => {
     res.status(500).json({
