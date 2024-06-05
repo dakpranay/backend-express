b52330a74bf8aae32ad8dc245d745792e342cf6a
dakpranay <97697949+dakpranay@users.noreply.github.com>
2024-06-05 21:48:50 +0530

    send the email templet on the user email using mailjet

--- utils/email.js
+++ utils/email.js
@@ -1,29 +1,65 @@
-const nodemailer=require('nodemailer')
+const nodemailer = require('nodemailer')
+const pug = require('pug')
+const htmlToText = require('html-to-text')
 
-const sendEmail=async options=>{
-    //create a transporter
-    const transporter=nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email,
+      this.firstName = user.name.split(' ')[0]
+    this.url = url,
+      this.from = `Pranay Dak <${process.env.EMAIL_FROM}>`
+  }
+
+  newTransporter() {
+    if (process.env.NODE_ENV === 'production') {
+      return nodemailer.createTransport({
+        host: 'in-v3.mailjet.com',
+        port: 587,
         auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD
+          user: '7f748f7a6493d5472670c64407b2fa39',
+          pass: 'e56ef347bacf6e85b61838a3892dea46'
         }
       });
-    //define the email options
-    const mailOptions={
-        from:'pranay <pranay@gmail.com>',
-        to:options.email,
-        subject:options.subject,
-        text:options.message
     }
 
-    //actually send the mail
-   try{
-    await transporter.sendMail(mailOptions)
-   }catch(err){
-    console.log("error in email.js file with send email")
-   }
-}
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
 
-module.exports=sendEmail
+  //send the actual email
+  async send(template, subject) {
+    // 1) render the html based on a pug templet
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject
+    })
+
+    // define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.htmlToText(html)
+    }
+
+    // 3 create a transport and send email
+    await this.newTransporter().sendMail(mailOptions)
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the natours family!')
+  }
+
+  async sendPasswordReset(){
+    this.send('passwordReset','Your password reset token(valid for only 10 min) ')
+
+  }
+}
